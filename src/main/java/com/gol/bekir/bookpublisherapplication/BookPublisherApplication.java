c986f8fdf22991839c82021315949f6e91162fd4
Bekir <bekirgol20@gmail.com>
2023-05-06 00:05:01 +0300

    Initial Commit

---
+++ src/main/java/com/gol/bekir/bookpublisherapplication/BookPublisherApplication.java
@@ -0,0 +1,9 @@
+package com.gol.bekir.bookpublisherapplication;
+
+import org.springframework.boot.SpringApplication;
+
+public class BookPublisherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookPublisherApplication.class, args);
+    }
+}
